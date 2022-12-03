292fb9391fd79431b82cce748abcb877c94b17ce
deepesh275 <deepeshkumar384@gmail.com>
2022-12-03 13:33:05 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [dogbread, setDogBread] = useState("affenpinscher")
+  const [dogbreadimag, setDogBreadimg] = useState("")
+  const [error, setError] = useState(false)
+  const [errormessage, setErrorMessage] = useState('')
+  
+  function newFun() {
+    setError(false)
+    axios.get(`https://dog.ceo/api/breed/${dogbread}/images/random`)
+    .then(function (response) {
+      // handle success
+      setDogBreadimg(response.data.message)
+    })
+    .catch(function (error) {
+      // handle error
+      if (error.response.data.code === 404) {
+        setError(true)
+        setErrorMessage(error.response.data.message)
+      }
+    })
+  }
+
+  useEffect(() => {
+
+    newFun();
+
+  }, [])
+ 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img style={{width:"400px", height:"400px"}} src={dogbreadimag} alt="" />
+      <div className='text'>
+        <input value={dogbread} onChange={(event) => setDogBread(event.target.value)}  className='input-text' type="text" placeholder='Enter Your Bread Name'/>
+        <button onClick={() => newFun()}  className='sub-btn'>Change</button>
+       {error === true ? <h3>{errormessage}</h3> : null }
+      </div>
     </div>
+         
   );
 }
 
